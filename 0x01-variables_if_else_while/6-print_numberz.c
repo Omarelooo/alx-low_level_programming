24362423270363c5539178e49999e01ef2bf51bf
Omarelooo <omark3012@gmail.com>
2023-07-14 15:54:03 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* DEscription */
+
+int main(void)
+{
+	char digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+		putchar('\n');
+		return (0);
+}
