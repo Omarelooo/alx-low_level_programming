ebfc2976254e76b127ab0947e4cb04f03dd9dc26
Omarelooo <omark3012@gmail.com>
2023-07-14 14:49:24 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* 
- * mmain 
+/**
+ * main - entry point 
  *
- * function
+ * Description: a
  *
- * return 0 
+ * Return: 0 (success)
  *
 */
 
