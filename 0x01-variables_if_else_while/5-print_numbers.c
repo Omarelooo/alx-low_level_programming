ed794995c96fa5e6569b4fcfc6a86d557566fc91
Omarelooo <omark3012@gmail.com>
2023-07-14 15:48:13 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* DEscription */
+
+int main(void)
+{
+        int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+        }
+        printf("\n");
+
+        return (0);
+}
