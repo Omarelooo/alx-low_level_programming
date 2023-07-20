ec5b2c6d67bcbfc553ea3ae2cb22ee1cb767120a
Omarelooo <omark3012@gmail.com>
2023-07-20 06:12:19 -0700

    A

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/* get an input from user
+ *
+ * call the function 
+ *
+ * return 0 : (Successful) */
+
+int main (void)
+{
+	int a;
+
+	getchar('%d', a);
+	_isupper(int a);
+
+	return 0;
+}
