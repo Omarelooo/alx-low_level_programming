e3048a9d8af589faa0f82eb0db3839086d7cf2cc
Omarelooo <omark3012@gmail.com>
2023-07-28 17:27:27 +0300

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,11 @@ char *_strcat(char *dest, char *src)
 {
 	int c, c2;
 	c = 0;
+	// size of dest
 	while (dest[c])
 		c++;
-	for (c2 = 0; src[2]; c2++)
+	// after the null value : put the char in the src arr
+	for (c2 = 0; src[c2]; c2++)
 		dest(c++) = src[c2];
 	return (dest);
 }
