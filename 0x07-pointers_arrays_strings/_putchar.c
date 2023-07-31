6fdf69cb06541b8a4fcfd7ce511704bf74dc1c9f
Omarelooo <omark3012@gmail.com>
2023-07-31 15:48:50 -0700

    a

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -2,9 +2,5 @@
 
 int _putchar(char c)
 {
-<<<<<<< HEAD
 	return (write(1, &c, 1));
-=======
-    return (write(1, &c, 1));
->>>>>>> 3a5cfe24c35ea5f5cd8e2a36c068f8f9457ccf16
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,7 +5,3 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 
 #endif
-<<<<<<< HEAD
-=======
-
->>>>>>> 3a5cfe24c35ea5f5cd8e2a36c068f8f9457ccf16
