00e63c40edfab3afdf10999b8461d1bd0de431dd
Omarelooo <omark3012@gmail.com>
2023-07-31 20:40:49 +0000

    a

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_memset function
+ * 
+ * return (0): success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x;
+	/* Number of chracters will change */
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+	return (s);
+}
+
