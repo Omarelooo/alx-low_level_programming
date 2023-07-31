b47a47237c98cd3724fc57d8b37633918f7c9676
Omarelooo <omark3012@gmail.com>
2023-08-01 02:09:36 +0300

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
-char *_memset(char *s, char b, unsigned int n)
-{
-    int x;
-    for (x = 0; x < n; x++)
-    {
-        s[x] = b;
-    }
-    return (s);
-=======
-#include <stdio.h>
-
-/* function _memset : change number of chracter with 1 charatcer */
 
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -24,5 +10,4 @@ char *_memset(char *s, char b, unsigned int n)
 		s[x] = b;
 	}
 	return (s);
->>>>>>> 6fdf69cb06541b8a4fcfd7ce511704bf74dc1c9f
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define MAIN_H
 #ifndef MAIN_H
 
@@ -7,13 +6,3 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
-
-=======
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-
-#endif
->>>>>>> 6fdf69cb06541b8a4fcfd7ce511704bf74dc1c9f
