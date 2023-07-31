2c62a2f1a3c6a0fbbb8378b43799d8f7dbca63c6
Omarelooo <omark3012@gmail.com>
2023-07-31 15:16:24 -0700

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
+/* function _memset : change number of chracter with 1 charatcer */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int x;
 	/* Number of chracters will change */
 	for (x = 0; x < n; x++)
 	{
+		/* change the chracter with b */
 		s[x] = b;
 	}
 	return (s);
