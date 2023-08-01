5778bbde17f6e8833050a27c1cf0cd664815e88d
Omarelooo <omark3012@gmail.com>
2023-08-01 19:09:02 +0300

    a

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,11 +3,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int x;
-	x = 0;
-	while (src[x] != 0 ; x < n)
+	while (x = 0; x < n; x++)
 	{
 		dest[x] = src[x];
-		x++;
 	}
 	return (dest);
 }
