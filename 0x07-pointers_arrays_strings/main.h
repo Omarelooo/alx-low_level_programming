d06daf9ffab3d301c3369f04acb370e4d155ba26
Omarelooo <omark3012@gmail.com>
2023-08-01 23:47:22 +0300

    a

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,11 @@
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif
