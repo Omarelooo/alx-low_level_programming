0db9035a207c0cfab81e64439dc8f2e5f93da87e
Omarelooo <omark3012@gmail.com>
2023-08-04 16:08:48 -0700

    A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 
+ * @argc : number of commands
+ * @argv : pointer
+ * return : 0
+ */
+
+int main(int argc, char *argv[]) __attribute__((unused))
+{
+	printf("%d\n", argc - 1);
+	return (0)
+}
