aa4ebc3cc4bb56386d78c461322ac536a632814b
Omarelooo <omark3012@gmail.com>
2023-08-09 07:52:36 -0700

    a

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print numbers 
- * @argc : number of commands
- * @argv : pointer
- * return : 0
+ * main - print numbers of arguments passed
+ * @argc: number of commands
+ * @argv: pointer
+ * Return: 0- sucess, non-zero-fail.
  */
 
-int main(int argc, char *argv[]) __attribute__((unused))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-	return (0)
+	return (0);
 }
