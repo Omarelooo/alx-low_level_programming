7ab79f4286dc8808067375b14cc107492312007b
Omarelooo <omark3012@gmail.com>
2023-08-14 17:20:07 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,11 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
