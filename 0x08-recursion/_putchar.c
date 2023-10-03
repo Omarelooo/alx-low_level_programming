32f56e3807464b0b2333901539cefa74f9f4dc1e
Omarelooo <omark3012@gmail.com>
2023-10-03 07:28:40 -0700

    a

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 #include "main.h"
 #include <unistd.h>
-=======
-#include <unistd.h>
 
->>>>>>> eec0c887c8a9b713527b853067874152a6ea4189
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
+ *
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ **/
 int _putchar(char c)
 {
 		return (write(1, &c, 1));
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
@@ -11,9 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-=======
 int _putchar(char c);
 void _puts_recursion(char *s);
->>>>>>> eec0c887c8a9b713527b853067874152a6ea4189
 
 #endif
