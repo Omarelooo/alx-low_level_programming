4bf84b253b19ca81db3c30737be55d61a7ce3f49
Omarelooo <omark3012@gmail.com>
2023-11-14 09:57:50 -0800

    A

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,25 @@
+#include "calc.h"
+
+int op_add(int a, int b)
+{
+	    return (a + b);
+}
+
+int op_sup(int a, int b)
+{
+	    return (a - b);
+}
+int op_mul(int a, int b)
+{
+	    return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	    return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	    return (a % b);
+}
