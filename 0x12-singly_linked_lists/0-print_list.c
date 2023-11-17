dfa20f311caf2386534f2cdfb9ba1072af80710f
Omarelooo <omark3012@gmail.com>
2023-11-17 15:28:46 -0800

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements
+ *  *
+ *   * @h: The list
+ *    *
+ *     * Return: Number of elements in the list
+ **/
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
